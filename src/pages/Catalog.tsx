98aff3514344ce7a31b6b7e0ce527ef22e45ef04
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-08-05 17:09:33 -0300

    Catalog page/ Product Card layout

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ProductCard } from '../components';
+import productImg from '../assets/product.png';
+import { ScrollView } from 'react-native-gesture-handler';
+import { theme } from '../styles';
+
+const products = [
+    {
+        id: 1,
+        name: "Computador Desktop - Intel Core i7",
+        imgUrl: productImg,
+        price: 2779.0,
+    },
+    {
+        id: 2,
+        name: "Computador Desktop - Intel Core i7",
+        imgUrl: productImg,
+        price: 2779.0,
+    },
+    {
+        id: 3,
+        name: "Computador Desktop - Intel Core i7",
+        imgUrl: productImg,
+        price: 2779.0,
+    },
+    {
+        id: 4,
+        name: "Computador Desktop - Intel Core i7",
+        imgUrl: productImg,
+        price: 2779.0,
+    },
+    {
+        id: 5,
+        name: "Computador Desktop - Intel Core i7",
+        imgUrl: productImg,
+        price: 2779.0,
+    },
+];
 
 const Catalog: React.FC = () => {
     return (
-        <View>
-            <Text>
-                Bem vindo ao Catálogo
-            </Text>
-        </View>
+        <ScrollView contentContainerStyle={theme.scrollContainer}>
+            {products.map((product) => (
+                <ProductCard {...product} key={product.id} />
+            ))}
+        </ScrollView>
     );
 }
 

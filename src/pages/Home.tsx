383480a71c5c532a9a921fbd0a3822d6c4b36139
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-08-05 16:14:23 -0300

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { theme, text } from '../styles';
+import arrow from '../assets/arrow.png';
+import draw from '../assets/draw.png';
 
-const Home: React.FC = ({ navigation }) => {
+const Home: React.FC = () => {
+    const navigation = useNavigation();
+    const catalog = "Catalog" as never;
+
     return (
-        <View>
-            <Text>
-                Bem vindo ao App
-            </Text>
-            <TouchableOpacity onPress={() => navigation.navigate('Catalog')}>
-                <Text>Clique aqui</Text>
-            </TouchableOpacity>
+        <View style={theme.container}>
+            <View style={theme.card}>
+                <Image source={draw} style={theme.draw} />
+                <View style={theme.textContainer}>
+                    <Text style={text.bold}>
+                        Conheça o melhor catálogo de produtos
+                    </Text>
+                    <Text style={text.regular}>
+                        Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.
+                    </Text>
+                </View>
+                <TouchableOpacity
+                    style={theme.primaryButton}
+                    activeOpacity={0.8}
+                    onPress={() => navigation.navigate(catalog)}
+                >
+                    <Text style={text.primaryText}>
+                        INICIE AGORA A SUA BUSCA
+                    </Text>
+                    <View style={theme.arrowContainer}>
+                        <Image source={arrow} />
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }

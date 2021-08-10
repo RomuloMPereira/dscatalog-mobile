06575d1446f767378d9299dd17ca35128a71cbbf
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-08-10 15:35:19 -0300

    Login page layout

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -70,6 +70,13 @@ const text = StyleSheet.create({
         color: colors.mediumGray,
 
     },
+    loginTitle: {
+        fontSize: 30,
+        color: colors.darkGray,
+        fontWeight: "400",
+        textTransform: "uppercase",
+        marginBottom: 50,
+    }
 });
 
 const theme = StyleSheet.create({
@@ -224,6 +231,41 @@ const theme = StyleSheet.create({
         borderWidth: 0.5,
         borderRadius: 10,
         borderColor: colors.lightGray,
+    },
+    //Login Page
+    loginCard: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: colors.white,
+        borderRadius: 20,
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    form: {
+        marginVertical: 10,
+    },
+    passwordContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 25,
+    },
+    textInput: {
+        width: 290,
+        height: 50,
+        borderWidth: 1,
+        borderColor: colors.mediumGray,
+        borderRadius: 10,
+        padding: 10,
+    },
+    toggle: {
+        marginLeft: -40,
     }
 });
 
